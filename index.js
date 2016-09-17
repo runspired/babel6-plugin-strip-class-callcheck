240abd726d654e5c1d0cd86effdb0408caf7b25d
Chris Thoburn <cthoburn@linkedin.com>
2016-09-17 01:15:51 -0700

    initial release

---
+++ index.js
@@ -0,0 +1,27 @@
+function stripClassCallCheck(babel) {
+  return new babel.Plugin('strip-class-call-check', {
+    visitor: {
+      ExpressionStatement: function(node, path) {
+        if (node.expression.type === 'CallExpression' &&
+          node.expression.callee &&
+          node.expression.callee.type === 'MemberExpression' &&
+          node.expression.callee.object.name === 'babelHelpers' &&
+          node.expression.callee.property.name === 'classCallCheck') {
+
+          if (path.body && path.body.length) {
+            path.body = path.body.filter(function(item) {
+              return item !== node;
+            });
+          }
+        }
+      }
+    }
+  });
+}
+
+stripClassCallCheck.baseDir = function() {
+  return __dirname;
+};
+
+
+module.exports = stripClassCallCheck;
